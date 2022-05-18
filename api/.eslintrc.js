841c585bf884dd72a7c0afdbc5cf2376aec6bd1f
RafaelAugustScherer <rafaelaugustscherer@gmail.com>
2022-05-18 16:35:11 -0300

    Update: App is now a Class component + Environment validation

--- api/.eslintrc.js
+++ api/.eslintrc.js
@@ -1,5 +1,9 @@
 module.exports = {
-    'extends': 'eslint:recommended',
+    'extends': [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+    ],
     'env': {
         'browser': false,
         'es2021': true,
--- api/app/index.ts
+++ api/app/index.ts
@@ -1,14 +1,34 @@
 import 'dotenv/config';
-import express from 'express';
+import express, { Router } from 'express';
+import cors from 'cors';
 import appRouter from './router';
+import { checkEnvironment } from './utils/app';
 
-const app = express();
-const APP_PORT = process.env.APP_PORT || 3000;
+class App {
+  public router: Router;
+  public app = express();
 
-app.use(express.json());
-app.use(appRouter);
+  constructor(router: Router) {
+    this.router = router;
+    this.setup();
+  }
 
-app.listen(
-  APP_PORT,
-  () => console.log(`Api is running at port ${APP_PORT}`),
-);
+  private setup() {
+    this.app.use(cors());
+    this.app.use(express.json());
+    this.app.use(this.router);
+  }
+
+  public start() {
+    checkEnvironment();
+
+    const APP_PORT = process.env.APP_PORT || 3000;
+    this.app.listen(
+      APP_PORT,
+      () => console.log(`Api is running at port: ${APP_PORT}`),
+    );
+  }
+}
+
+const app = new App(appRouter);
+app.start();
